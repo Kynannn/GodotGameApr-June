d76de970073b206a336a3be3fb904e51571a75c6
Kynannn <Kool000321@outlook.com>
2024-05-09 11:46:28 +0800

    added in cutscene (why doesnt audio work)

--- cutscene_opening.gd
+++ cutscene_opening.gd
@@ -4,3 +4,7 @@ extends VideoStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	BgMusic.stop()
+
+
+func _on_finished():
+	get_tree().change_scene_to_file("res://main_game.tscn")
--- menu.gd
+++ menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://main_game.tscn")
+	get_tree().change_scene_to_file("res://cutscene_opening.tscn")
 
 
 func _on_options_pressed():
