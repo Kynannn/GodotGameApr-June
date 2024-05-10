71bf3032d82d0b21c48a67262689a18d7d4e1561
Kynannn <Kool000321@outlook.com>
2024-05-10 08:46:32 +0800

    added skip button to cutscene

--- cutscene_opening.gd
+++ cutscene_opening.gd
@@ -5,6 +5,9 @@ extends VideoStreamPlayer
 func _ready():
 	BgMusic.stop()
 
-
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://main_game.tscn")
+	
 func _on_finished():
 	get_tree().change_scene_to_file("res://main_game.tscn")
