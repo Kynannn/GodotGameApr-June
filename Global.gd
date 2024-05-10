bf8c912532101c4c21beb50d4381cb29f7a7422b
Kynannn <Kool000321@outlook.com>
2024-05-10 09:18:37 +0800

    hell yeah the timer resets now

--- Global.gd
+++ Global.gd
@@ -2,3 +2,10 @@ extends Node
 
 var speedrun_time = 0
 var complete_time = "0"
+
+signal ResetTimer()
+
+func _process(_delta):
+	if Input.is_action_just_released("reset"):
+		Global.speedrun_time = 0.0
+		get_tree().reload_current_scene()
